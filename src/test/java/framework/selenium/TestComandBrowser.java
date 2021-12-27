d920baa5a0c91f971218dd5a1ee96e6aeafd5986
AndersonDiasSampaio <ads@ic.ufal.br>
2021-12-27 15:57:41 -0300

    basic locators

--- src/test/java/framework/selenium/TestComandBrowser.java
+++ src/test/java/framework/selenium/TestComandBrowser.java
@@ -7,33 +7,52 @@ import org.openqa.selenium.chrome.ChromeDriver;
 import org.openqa.selenium.interactions.Actions;
 
 public class TestComandBrowser {
-	
-	 static WebDriver driver;
-	   public static void main( String[] args )
-	    {
-		   comandbrowser();
-	    }
-	   public static void comandbrowser() {
-		   System.setProperty("webdriver.chrome.driver", "C:/autodrivers/chromedriver.exe");
-		   driver= new ChromeDriver();
-		   driver.manage().window().maximize();
-		   driver.get("https://pt.stackoverflow.com/"); // site não pode estar logado se não o naveagdor entende como pishin
-		   driver.navigate().to("https://www.facebook.com/");
-		   driver.findElement(By.id("email")).sendKeys("anderson.dias.sampaio@hotmail.com");
-		   driver.findElement(By.id("pass")).sendKeys("tomanocU13");
-		   Actions act = new Actions(driver);
-		   act.sendKeys(Keys.ENTER).build().perform();
-		  // driver.findElement(By.linkText("Imagens")).click();
-		   driver.navigate().back();
-		   driver.navigate().forward();
-		   String titulo = driver.getTitle();
-		   System.out.println(titulo);
-		   titulo= driver.getCurrentUrl();
-		   System.out.println(titulo);
-		  // System.out.println(titulo);
-		   
-		 
-		  
 
-	   }
+	static WebDriver driver;
+
+	public static void main(String[] args) {
+		comandbrowser();
+	}
+
+	public static void comandbrowser() {
+		System.setProperty("webdriver.chrome.driver", "C:/autodrivers/chromedriver.exe");
+		driver = new ChromeDriver();
+		driver.manage().window().maximize();
+		driver.get("https://pt.stackoverflow.com/"); // site não pode estar logado se não o naveagdor entende como
+														// pishin
+		driver.navigate().to("https://www.facebook.com/");
+		boolean teste;
+		
+		teste = driver.findElement(By.id("email")).isDisplayed();
+		System.out.println(teste);
+		String texto;
+		texto=driver.findElement(By.linkText("Esqueceu a senha?")).getText();
+		System.out.println(texto);
+		driver.findElement(By.name("email")).sendKeys("anderson.dias.sampaio@hotmail.com");
+		//driver.findElement(By.id("email")).sendKeys("anderson.dias.sampaio@hotmail.com");
+		driver.findElement(By.id("pass")).sendKeys("tomanocU13");
+		driver.findElement(By.linkText("Esqueceu a senha?")).getText();
+		
+		Actions act = new Actions(driver);
+		act.sendKeys(Keys.ENTER).build().perform();
+		// driver.findElement(By.linkText("Imagens")).click();
+		driver.navigate().back();
+		driver.navigate().forward();
+		String titulo = driver.getTitle();
+		System.out.println(titulo);
+		titulo = driver.getCurrentUrl();
+		System.out.println(titulo);
+		try {
+			Thread.sleep(10000);
+		} catch (InterruptedException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
+
+		System.out.println(titulo);
+
+		driver.quit();
+		// System.out.println(titulo);
+
+	}
 }
